37596318769c86a12b979928069761937a7aaa05
ishaanvidhariwal <ishaanvi.dhariwal@gmail.com>
2026-09-16 14:48:22 +0530

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_KEY = "YOUR_SUPABASE_PUBLISHABLE_KEY";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+

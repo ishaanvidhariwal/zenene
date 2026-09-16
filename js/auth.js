2fd512553b4cceaf7539b334e83340314da3c749
ishaanvidhariwal <ishaanvi.dhariwal@gmail.com>
2026-09-16 14:51:55 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,11 @@
+async function getCurrentUser() {
 
+    const { data, error } =
+        await supabaseClient.auth.getUser();
+
+    if (error || !data.user) {
+        return null;
+    }
+
+    return data.user;
+}
